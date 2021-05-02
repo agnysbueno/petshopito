5ff4c3e47270e55f4624b2b2826e911587344f4a
Larissa Diniz <larissa.diniz_23@hotmail.com>
2021-05-02 17:41:02 -0300

    new pet strategy

--- db/create-pet.php
+++ db/create-pet.php
@@ -1,29 +1,77 @@
 <?php
+include_once("db.php");
 
-    include_once("db.php");
+$msg = '';
 
-    if(!empty($_POST))
-    {
-        $nome = $_POST['name'];
-        $especie = $_POST['specie'];
-        $raca = $_POST['breed'];
-        $idade = $_POST['age'];
-        $cor = $_POST['color'];     
+if(!empty($_POST))
+{
 
-        $sql = "INSERT INTO denan241_designBarueri.projeto11_cadastro_pets(nome,especie,raca,idade,cor) 
-    VALUES ('${nome}', '${especie}', '${raca}', '${idade}', '${cor}');";
-
-    echo $sql;
-    $query = $mysqli->query($sql);
+	$nome = $_POST['name'];
+    $especie = $_POST['specie'];
+    $raca = $_POST['breed'];
+    $idade = $_POST['age'];
+    $cor = $_POST['color'];
+    $fk = $_POST['id'];
     
-    if ($query){
-            echo "<br>Cadastrado com sucesso!";
-        }
-        else{
-        echo "<br>a query eh: $sql";
-            echo "<br>Houve um erro no cadastrado !";
-        }
 
-    }
-
+    $sql = "INSERT INTO projeto11_cadastro_pets(nome, especie, raca, idade, cor, fk_id_usuario) 
+	VALUES ('${nome}', '${especie}', '${raca}', '${idade}', '${cor}', ${fk});";
+	
+$query = $mysqli->query($sql);
+ 
+if ($query){
+		$msg = "Cadastro efetuado com sucesso!";
+	}
+	else{
+		$msg = "Houve um erro no cadastro! Tente novamente";
+	}
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <link rel="shortcut icon" href="../../public/assets/icons/coin.ico">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../icons/icon.ico"/>
+        <title>Petshopito | Novo Pet</title>
+
+		<!-- CSS próprio -->
+		<link rel='stylesheet' type='text/css' media='screen' href='../styles/main.css'>
+    	<link rel='stylesheet' type='text/css' media='screen' href='../styles/messages.css'>
+    </head>
+
+	<body>
+		<div class="message message-pet">
+			<?php
+				if ($msg == "Cadastro efetuado com sucesso!") {
+			?> 	
+				<div class="create create-pet">
+					<img src="../icons/checked.svg" alt="" width="50px"
+						onload="redirect('new-pet.html')">
+					<p class="pet"><?=$msg?></p>
+				</div>
+			<?php            
+				} else {
+			?>  
+				<div class="create create-pet">
+					<img src="../icons/error.svg" alt="" width="50px"
+						onload="redirect('new-pet.html')">
+					<p class="pet"><?=$msg?></p>
+				</div>
+			<?php            
+				}
+			?> 	
+		</div>
+
+		<script>
+            function redirect(page){
+                setTimeout(function(){
+					location = `http://localhost/petshopito/${page}`
+					},5000)
+            }
+        </script>  
+	</body>
+
+</html>
